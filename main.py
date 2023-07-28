5c79ccd133c7fc6f8b57872a19799b45c1a8a3a5
ekusiadadus <ekusiadadus@gmail.com>
2023-07-28 18:38:54 +0900

    feat: :fire: add api key

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import openai
 
+OPENAI_API_KEY = "YOUR_API_KEY"
+
 file = open("./data/test.mp3", "rb")
-transcription = openai.Audio.transcribe("whisper-1", file)
+transcription = openai.Audio.transcribe("whisper-1", file, api_key=OPENAI_API_KEY)
 
 print(transcription)
